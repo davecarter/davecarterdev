0f8007e09836351c19b0572a8af2ba1a50619636
David G <david.garcia@scmspain.com>
2019-04-17 18:51:24 +0200

    add registered sw

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Davecarter.dev')
+console.log("Davecarter.dev");
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./serviceworker.js").then(
+      registration => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      function(err) {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}

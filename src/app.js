1772fb4cbc713eb62f7fab3b3a6cba2395e8012a
Carlos Villuendas <carlosvillu@gmail.com>
2019-04-20 17:19:35 +0200

    feat(Root): add push notification

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {register} from '@s-ui/bundler/registerServiceWorker'
+import firebase from 'firebase/app'
+import 'firebase/messaging';
 import './index.scss'
 
 const App = () => {
@@ -23,3 +25,43 @@ register({
   first: () => window.alert('Content is cached for offline use.'),
   renovate: () => window.alert('New content is available; please refresh.')
 })()
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCL0z872M0WFdd3Cgxx_42tHiJn8DVMjqw",
+    authDomain: "davecarterdev.firebaseapp.com",
+    databaseURL: "https://davecarterdev.firebaseio.com",
+    projectId: "davecarterdev",
+    storageBucket: "davecarterdev.appspot.com",
+    messagingSenderId: "876465390296"
+  };
+
+
+  const askForPermissioToReceiveNotifications = async () => {
+    try {
+      const messaging = firebase.messaging();
+      await messaging.requestPermission();
+      const token = await messaging.getToken();
+      console.log('token do usuário:', token);
+      
+      return token;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+const inicializarFirebase = () => {
+    firebase.initializeApp({
+      messagingSenderId: "876465390296"
+    });
+  navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then((registration) => {
+        firebase.messaging().useServiceWorker(registration);
+        const token = askForPermissioToReceiveNotifications()
+      });
+  }
+
+
+inicializarFirebase()
+
